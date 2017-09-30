4428a193e281bb901ae357d32d020d98b795bf25
NovusDundus <joshua.dagostino@outlook.com>
2017-09-30 23:27:11 +1000

    - Added unmodified XInput package. - Iterated on Greybox (Created V3_001) - Began prototyping (Fireball, shooting)

---
+++ Sector XII/Assets/Magicide/Scripts/Prototyping/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+	public float bulletSpeed;
+	public float fireRate;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+
+	public float nextFire;
+
+	public KeyCode shoot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		PControls ();
+	}
+
+	void PControls() {
+		
+		if (Input.GetKey (shoot) && Time.time > nextFire) {
+			nextFire = fireRate + Time.time;
+			GameObject GO = Instantiate (shot, shotSpawn.position, Quaternion.identity) as GameObject;
+			GO.GetComponent<Rigidbody> ().AddForce (shotSpawn.transform.forward * bulletSpeed, ForceMode.Impulse);
+		}
+	}
+}
+
+
