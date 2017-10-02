2fa52ceb3e07dcac00843d6f20f06618f8a7d900
CallenMitchell <32442472+CallenMitchell@users.noreply.github.com>
2017-10-02 16:16:43 +1100

    Added AI Attack and Follow scrips

---
+++ Sector XII/Assets/Magicide/Scripts/AI/AIAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttack : MonoBehaviour {
+    public int _Damage = 1;
+    public float _attackSpeed = 1;
+    public float _attackCooldown = 1;
+    public float _attackRange = 1;
+    private PlayerCharacter attackPlayer;
+    CircleCollider2D _attackAOE;
+    bool canattack = true;
+    float attackdelay = 0f;
+    float attackRate = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //attackPlayer = GetComponent<HumanoidCharacter>();
+        //gameObject.Pla
+        _attackAOE.radius = 5;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        attackdelay -= Time.deltaTime;
+
+        _attackAOE.transform.position = transform.position;
+
+        if (_attackAOE.OverlapPoint(attackPlayer.transform.position)) {
+
+            if (attackdelay <= 0f)
+            {
+                attack();
+            }
+        }
+	}
+
+    void attack()
+    {
+        attackPlayer.Damage(_Damage);
+        attackdelay = attackRate;
+    }
+}
---
+++ Sector XII/Assets/Magicide/Scripts/AI/AIFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    private Transform target;
+    public float moveSpeed;
+    
+    void Start()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+      
+	}
+}
