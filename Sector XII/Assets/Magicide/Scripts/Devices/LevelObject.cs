eb7f762575d7ce2389408e143e1838bbd4739909
DanMarton <dmarton94@gmail.com>
2017-10-10 10:29:09 +1100

    - Added levelobject script

---
+++ Sector XII/Assets/Magicide/Scripts/Devices/LevelObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObject : MonoBehaviour {
+
+    ///--------------------------------------///
+    /// Created by: Daniel Marton
+    /// Created on: 10.10.2017
+    ///--------------------------------------///
+
+    //----------------------------------------------------------------------------------
+    // *** VARIABLES ***
+
+    /// Public (designers)
+    public bool _StaticObject = true;
+
+    /// Private
+    private Collider _Collision;
+
+    //--------------------------------------------------------------
+    // *** CONSTRUCTORS ***
+    
+    void Start () {
+
+        // Get reference to collision
+        _Collision = GetComponent<Collider>();
+
+        // Add to object pool
+        if (_StaticObject == true) {
+
+            // Static object pool
+            LevelManager._pInstance.GetStaticObjects().Add(this);
+        }
+
+        else { /// _StaticObject == false
+
+            // Dynamic object pool
+            LevelManager._pInstance.GetDynamicObjects().Add(this);
+        }
+	}
+
+    //--------------------------------------------------------------
+    // *** MISC ***
+
+    void Update () {
+		
+	}
+
+    public Collider GetCollision() {
+
+        return _Collision;
+    }
+}
