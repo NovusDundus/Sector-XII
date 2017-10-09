5934145064d6b9da2cee907feb92331e160b0d30
DanMarton <dmarton94@gmail.com>
2017-10-09 21:03:14 +1100

    - added toggle dev ui

---
+++ Sector XII/Assets/Magicide/Scripts/Debug/Debug_ShowDevUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_ShowDevUI : MonoBehaviour {
+
+    public GameObject _Panel;
+    public Character _PlayerOne;
+
+    private bool PanelShowing = true;
+    private bool pauseInitiated = false;
+    private float _TimerActive = 0f;
+    private float _TimerInactive = 0f;
+
+    void Start () {
+        
+	}
+	
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+
+        if (PanelShowing == true) {
+
+            _TimerActive += Time.deltaTime;
+            _TimerInactive = 0f;
+        }
+        else {
+
+            _TimerInactive += Time.deltaTime;
+            _TimerActive = 0f;
+        }
+
+        if (_PlayerOne._Player.GetSpecialLeftButton == true) {
+
+            if (PanelShowing == true && _TimerActive > 0.5f) {
+
+                _Panel.SetActive(false);
+                PanelShowing = false;
+            }
+
+            else if (_TimerInactive > 0.5f){
+
+                _Panel.SetActive(true);
+                PanelShowing = true;
+            }
+        }
+    }
+}
