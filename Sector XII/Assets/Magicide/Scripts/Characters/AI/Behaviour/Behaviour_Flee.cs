72c39ad3d97408964b696c9fe9fdb527f6984b69
DanMarton <dmarton94@gmail.com>
2017-10-09 19:43:10 +1100

    - removed AiFleeScript (Behaviour_Flee is its replacement!)
    - removed ai attack and ai follow scripts (unused)

--- Sector XII/Assets/Magicide/Scripts/Characters/AI/Behaviour/Behaviour_Flee.cs
+++ Sector XII/Assets/Magicide/Scripts/Characters/AI/Behaviour/Behaviour_Flee.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class AiFleeScript : MonoBehaviour
-{
-    private float FleeDistance; //how close/far the AI is going to get before running away from the player
-    public float FleeOffset; //the distance between the player and the enemy
-    public float FleeThreshold; //determains the distance the AI will run(Once it is outside of the determained threshold it will stop running)
-    public float movespeed;
+public class Behaviour_Flee : MonoBehaviour {
 
-    public GameObject m_Target; //references the game object in the game]
+    ///--------------------------------------///
+    /// Created by: Callen Mitchel
+    /// Created on: 9.10.2017
+    ///--------------------------------------///
 
-    NavMeshAgent agent; //sets a variable name to be used in the rest of the script
-    // Use this for initialization
+    //----------------------------------------------------------------------------------
+    // *** VARIABLES ***
+
+    private float FleeDistance;                                     // how close/far the AI is going to get before running away from the player
+    public float FleeOffset;                                        // the distance between the player and the enemy
+    public float FleeThreshold;                                     // determains the distance the AI will run(Once it is outside of the determained threshold it will stop running)
+    public float movespeed;                                            
+                                                                       
+    public GameObject m_Target;                                     // references the game object in the game (target to flee from)
+                                                                       
+    NavMeshAgent agent;                                             // sets a variable name to be used in the rest of the script
+
+    //--------------------------------------------------------------
+    // *** CONSTRUCTORS ***
+
     void Start()
     {
-        //uses the variable called agent, then uses getcomponent to determain what component is going to be placed into the agent
+        // Uses the variable called agent, then uses getcomponent to determain what component is going to be placed into the agent
         agent = GetComponent<NavMeshAgent>();
     }
+
+    //--------------------------------------------------------------
+    // *** FRAME ***
 
     // FixedUpdate is called once per frame but with regular update intervals
     void FixedUpdate()
@@ -24,19 +38,19 @@ public class AiFleeScript : MonoBehaviour
         // Determine distance between ai and target
         FleeDistance = Vector3.Distance(m_Target.transform.position, agent.transform.position);
 
-        //if distance between player and enemy is less than a certain number
+        // If distance between player and enemy is less than a certain number
         if (FleeDistance <= FleeThreshold)
         {
-            //first thing this does it is find the position of the gameobject that this script is associated with(EnemyAI gameobject)
-            //the compiler then finds the position of m_target(Which is the player gameobject)
-            //then the compiler subtracts whatever the position of the player, by the position of the AI is and move it into the vecbetweenplayerandenemy variable
+            // First thing this does it is find the position of the gameobject that this script is associated with(EnemyAI gameobject)
+            // the compiler then finds the position of m_target(Which is the player gameobject)
+            // then the compiler subtracts whatever the position of the player, by the position of the AI is and move it into the vecbetweenplayerandenemy variable
             Vector3 vecBetweenPlayerAndEnemy = transform.position - m_Target.transform.position;
 
-            //finds the position of the script that is associated with the gameobject(EnemyAI)
-            //then adds the new position of the vecbetweenplayerandenemy
+            // finds the position of the script that is associated with the gameobject(EnemyAI)
+            // then adds the new position of the vecbetweenplayerandenemy
             Vector3 targetPosition = transform.position + vecBetweenPlayerAndEnemy.normalized * FleeOffset;
 
-            //this sets the destination for the AI to move to that position(in this case its the player)
+            // This sets the destination for the AI to move to that position(in this case its the player)
             agent.SetDestination(targetPosition);
         }
 
@@ -44,12 +58,6 @@ public class AiFleeScript : MonoBehaviour
         {
             Debug.Log("OUT OF RANGE");
         }
-        //{
-
-                                                 //  enemy pos - player pos
-           
-
-        //}
-
     }
+
 }
