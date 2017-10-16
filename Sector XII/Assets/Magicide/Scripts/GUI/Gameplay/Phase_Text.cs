7ab965cfa38677523e02ac9c2cb07ed84274b01b
DanMarton <dmarton94@gmail.com>
2017-10-16 12:10:41 +1100

    - Added script to allow text components to show on certain gameplay states

---
+++ Sector XII/Assets/Magicide/Scripts/GUI/Gameplay/Phase_Text.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Phase_Text : MonoBehaviour {
+
+    ///--------------------------------------///
+    /// Created by: Daniel Marton
+    /// Created on: 16.10.2017
+    ///--------------------------------------///
+
+    //----------------------------------------------------------------------------------
+    // *** VARIABLES *** 
+
+    /// Public (designers)
+    public MatchManager.GameState ShowInState;
+
+    /// Private
+    private Text _TitleText;
+
+    //--------------------------------------------------------------
+    // *** CONSTRUCTORS *** 
+
+    public void Start() {
+        
+        // Get reference to text component
+        _TitleText = GetComponent<Text>();
+    }   
+
+    //--------------------------------------------------------------
+    // *** FRAME *** 
+
+    public void FixedUpdate () {
+		
+        // If gameplay is in the correct phase
+        if (MatchManager._pInstance.GetGameState().Equals(ShowInState)) { 
+
+            // Show text
+            _TitleText.enabled = true;
+        }
+
+        // If gameplay isnt in the correct phase
+        else { /// MatchManager._pInstance.GetGameState() != (ShowInState)
+
+            // Hide text
+            _TitleText.enabled = false;
+        }
+    }
+
+}
