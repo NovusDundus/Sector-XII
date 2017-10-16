109c901fa5f28271099c6af5112cfdae9dd4972d
CallenMitchell <32442472+CallenMitchell@users.noreply.github.com>
2017-10-16 14:59:49 +1100

    - YOU CAN RESPAWN AI NOW WOO

--- Sector XII/Assets/Magicide/Scripts/Singletons/Managers/AiManager.cs
+++ Sector XII/Assets/Magicide/Scripts/Singletons/Managers/AiManager.cs
@@ -11,22 +11,25 @@ public class AiManager : MonoBehaviour {
 
     //----------------------------------------------------------------------------------
     // VARIABLES
-
+    
     /// Public (Designers)
     [Header(" *** WYRM CHARACTER ***")]
     [Header("- Health")]
     public int _WyrmStartingHealth = 100;                           // Starting health of the wyrms when spawning.
     [Header("- Movement")]
     public float _WyrmMovementSpeed = 5f;                           // Movement speed of the wyrm character.
+    [Header("- AI Respawning")]
+    public List<Transform> _spawnPoints;                            // Array of spawn points
+    public int _maxCharacters = 10;
 
     /// Public (internal)
     [HideInInspector]
     public static AiManager _pInstance;                             // This is a singleton script, Initialized in Startup().
-
+    
     /// Private
     private List<Character> _POOL_ALIVE_MINIONS;                    // Object pool of all ALIVE minions in the scene
-    private List<Character> _POOL_DEAD_MINIONS;                     // Object pool of all DEAD minions in the scene
-
+    public List<Character> _POOL_DEAD_MINIONS;                     // Object pool of all DEAD minions in the scene
+    
     //--------------------------------------------------------------
     // CONSTRUCTORS
 
@@ -44,7 +47,38 @@ public class AiManager : MonoBehaviour {
 
         // Create vector arrays
         _POOL_ALIVE_MINIONS = new List<Character>();
-        _POOL_DEAD_MINIONS = new List<Character>();
+        ///_POOL_DEAD_MINIONS = new List<Character>();
+        ///_spawnPoints = new List<Transform>();
+
+        //loop over all spawned minions  (_POOL_SPAWNED_MINIONS)
+            //set each one's position to a random spawn position     
+         //gives you random number between 1 and 20.
+
+            // testing respawn function
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
+        OnRespawn();
     }
 
     //--------------------------------------------------------------
@@ -60,6 +94,31 @@ public class AiManager : MonoBehaviour {
 
     //--------------------------------------------------------------
     // OBJECT POOLS
+
+    public void OnRespawn() {
+    
+        if (_POOL_DEAD_MINIONS.Count > 0 && _maxCharacters > 0)
+        {
+            // Get the character from the end of the dead array
+            Character newAi = _POOL_DEAD_MINIONS[_POOL_DEAD_MINIONS.Count - 1];
+
+            // Get random int (min = 0, max = vector array.size -1)
+            int randIndex = Random.Range(0, _spawnPoints.Count - 1); //change 10 to the size of the spawnPoints array - 1
+
+            // Get spawn position from spawnPoints [ randIndex ].position (newAI)
+            // Set ai transform's to the random spawn's position
+            newAi.transform.position = _spawnPoints[randIndex].position;
+
+            // Add ai (newAI variable) to active minion array
+           _POOL_ALIVE_MINIONS.Add(newAi);
+
+            // Remove ai (new AI variable) from dead minion array
+            _POOL_DEAD_MINIONS.RemoveAt(_POOL_DEAD_MINIONS.Count - 1);
+
+            // -1 from ai lives cap
+            _maxCharacters -= 1;
+        }        
+    }
 
     public List<Character> GetActiveMinions() {
 
