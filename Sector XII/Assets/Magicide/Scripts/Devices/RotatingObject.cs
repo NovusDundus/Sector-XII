cae47da75cc73d31283acb1f657f913bdfaecb1b
DanMarton <dmarton94@gmail.com>
2017-11-14 09:24:00 +1100

    - new rotating object script

---
+++ Sector XII/Assets/Magicide/Scripts/Devices/RotatingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+
+    ///--------------------------------------///
+    /// Created by: Alessandro Baiocchi
+    /// Created on: 14.11.2017
+    ///--------------------------------------///
+
+    //----------------------------------------------------------------------------------
+    // *** VARIABLES ***
+
+    /// Private
+    private float degrees = 15.0f;
+
+    //--------------------------------------------------------------
+    // *** FRAME ***
+    
+    private void Update () {
+
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degrees, 0f), Space.World);
+	}
+
+}
