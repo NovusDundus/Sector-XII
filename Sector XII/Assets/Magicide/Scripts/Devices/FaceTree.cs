5370b1d95bc810b1c1177d3f66aa987eb65981e0
DanMarton <dmarton94@gmail.com>
2017-11-14 14:33:01 +1100

    - added face tree script
    - deleted teleport room script

---
+++ Sector XII/Assets/Magicide/Scripts/Devices/FaceTree.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTree : MonoBehaviour {
+
+    ///--------------------------------------///
+    /// Created by: Daniel Marton
+    /// Created on: 14.11.2017
+    ///--------------------------------------///
+
+    //---------------------------------------------------------------------------------
+    // *** VARIABLES ***
+
+    /// Private
+    private List<AudioSource> _OnHitSounds;
+    private Collider _HitCollision;
+    private bool _PlayingSound = false;
+    private AudioSource _SoundBeingPlayed;
+
+    //--------------------------------------------------------------
+    // *** CONSTRUCTORS ***
+
+    void Start () {
+
+        _HitCollision = GetComponent<Collider>();
+        _OnHitSounds = SoundManager._pInstance._VOX_FaceTreeDialoglist;
+    }
+
+    //--------------------------------------------------------------
+    // *** HIT ***
+
+    public void Update() {
+
+        if (_PlayingSound == true) {
+
+            // Sound has finished playing
+            if (_SoundBeingPlayed.isPlaying == false) {
+
+                // Reset
+                _PlayingSound = false;
+            }
+        }
+    }
+
+    public void OnHit() {
+
+        // Not currently playing a sound
+        if (!_PlayingSound) {
+
+            // Get a random sound from the audio list
+            int i = Random.Range(0, _OnHitSounds.Count);
+            _SoundBeingPlayed = _OnHitSounds[i];
+            _SoundBeingPlayed.Play();
+            _PlayingSound = true;
+        }
+    }
+
+}
